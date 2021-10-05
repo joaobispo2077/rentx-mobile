59af77dc477eed92f7974896a7032bfb10b30944
João Bispo <joaobispo2077@gmail.com>
2021-10-05 15:23:35 -0300

    feat: setup svg transformer and types to use as component

---
+++ src/@types/svg/index.d.ts
@@ -0,0 +1,7 @@
+declare module '*.svg' {
+  import React from 'react';
+  import { SvgProps } from 'react-native-svg';
+
+  const SvgComponent: React.FC<SvgProps>;
+  export default SvgComponent;
+}
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,13 +1,26 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
 
   background-color: ${({ theme }) => theme.colors.background_primary};
 `;
-export const Title = styled.Text`
-  color: ${({ theme }) => theme.colors.main};
+
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFValue(113)}px;
+
+  padding: ${RFValue(69)}px ${RFValue(32)}px ${RFValue(16)}px;
+
+  background-color: ${({ theme }) => theme.colors.shape_dark};
+`;
+
+export const Logo = styled.View``;
+
+export const Text = styled.Text`
+  color: ${({ theme }) => theme.colors.text};
   font-family: ${({ theme }) => theme.fonts.primary_400};
+  font-size: ${RFValue(16)}px;
 `;
