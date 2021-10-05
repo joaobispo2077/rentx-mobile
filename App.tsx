db2f6864cade7dc4fc8460e3492fb84325f6393e
João Bispo <joaobispo2077@gmail.com>
2021-10-05 14:39:44 -0300

    feat: setup fonts

--- App.tsx
+++ App.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
+
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+  useFonts,
+} from '@expo-google-fonts/archivo';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import AppLoading from 'expo-app-loading';
+
 import { Home } from './src/screens/Home';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return <Home />;
 }
