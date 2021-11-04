c1f65f4afd81d661d81f87444c3a9e277c6f3e43
João Bispo <joaobispo2077@gmail.com>
2021-11-04 17:59:18 -0300

    feat: add dynamic size of logo svg into Scheduling screen

--- src/screens/SchedulingComplete/index.tsx
+++ src/screens/SchedulingComplete/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useWindowDimensions } from 'react-native';
 
 import DoneSvg from '../../assets/done.svg';
 import LogoSvg from '../../assets/logo_background_gray.svg';
@@ -11,10 +12,12 @@ import {
 } from './styles';
 
 export function SchedulingComplete() {
+  const { width } = useWindowDimensions();
+
   return (
     <Container>
-      <LogoSvg />
-      <DoneSvg />
+      <LogoSvg width={width} />
+      <DoneSvg width={80} height={80} />
       <SuccessTitle>Carro alugado!</SuccessTitle>
       <SuccessDescription>
         Agora você só precisa ir {'\n'} até a concessionária da RENTX {'\n'}{' '}
