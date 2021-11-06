e3629b8c4245cbb6fc1718e4875fe16f9054a2d6
João Bispo <joaobispo2077@gmail.com>
2021-11-06 11:48:52 -0300

    feat: add script to setup json server according machine ip address

---
+++ scripts/setupJsonServer.js
@@ -0,0 +1,54 @@
+/* eslint-disable import-helpers/order-imports */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const util = require('util');
+const os = require('os');
+const dns = require('dns');
+const child_process = require('child_process');
+
+const exec = util.promisify(child_process.exec);
+const lookup = util.promisify(dns.lookup);
+const path = require('path');
+
+const PORT = '3333';
+
+async function setupJsonServer() {
+  try {
+    console.log('Checking if JSON server is already running...');
+
+    const { stdout: stdoutCheckJsonServer } = await exec('netstat -aon');
+    const lines = stdoutCheckJsonServer.split(os.EOL);
+    const jsonServerPort = lines.find((line) => line.includes('json-server'));
+
+    if (jsonServerPort) {
+      console.log('JSON server is already running.');
+      return;
+    }
+
+    console.log('JSON server is not running.');
+    console.log('Setting up JSON server...');
+
+    const hostname = os.hostname();
+    const ip = await lookup(hostname);
+
+    console.log(`Setting up json server on ${ip.address}`);
+
+    await exec('dir');
+    const root = path.resolve(__dirname, '..');
+    console.log(`entry into path: ${root}`);
+    await exec(`cd ${root}`);
+
+    const command = `yarn json-server --host ${ip.address} ./src/services/server.json -p ${PORT}`;
+    console.log('Running command:', command);
+    console.log('Waiting for json server to start...');
+    console.log(`Access json server at http://${ip.address}:${PORT}...`);
+
+    await exec(command);
+  } catch (error) {
+    console.log('error... aborting setup');
+    console.log(error.message);
+    console.error(error);
+  }
+}
+
+setupJsonServer();
