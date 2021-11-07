fc2a9b24e408ff57516ef13689ad3aa707eb8854
João Bispo <joaobispo2077@gmail.com>
2021-11-07 12:32:32 -0300

    feat: add handle change date into Scheduling screen

--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
@@ -8,7 +8,8 @@ import { useTheme } from 'styled-components/native';
 import ArrowIcon from '../../assets/arrow.svg';
 import { BackButton } from '../../components/BackButton';
 import { Button } from '../../components/Button';
-import { Calendar } from '../../components/Calendar';
+import { Calendar, DayProps, CalendarProps } from '../../components/Calendar';
+import { generateInterval } from '../../components/Calendar/generateInterval';
 import { StackNavigatorParamList } from '../../routes/stack.routes';
 import {
   Container,
@@ -30,12 +31,47 @@ export function Scheduling() {
   const theme = useTheme();
   const navigation = useNavigation<SchedulingScreenNavigationProps>();
 
+  const [lastSelectedDate, setLastSelectedDate] = useState<DayProps>();
+  const [markedDates, setMarkedDates] =
+    useState<CalendarProps['markedDates']>();
+
   const handleNavigateGoBack = () => {
     navigation.goBack();
   };
 
   const handleNavigateToSchedulingDetails = () => {
     navigation.navigate('SchedulingDetails');
+  };
+
+  const getStartDate = (endDate: DayProps) => {
+    const startDate = lastSelectedDate?.timestamp ? lastSelectedDate : endDate;
+
+    if (startDate.timestamp > endDate.timestamp) {
+      return endDate;
+    }
+
+    return startDate;
+  };
+
+  const getEndDate = (date: DayProps, startDate: DayProps) => {
+    const endDate = date;
+
+    if (startDate?.timestamp > endDate?.timestamp) {
+      return startDate;
+    }
+
+    return endDate;
+  };
+
+  const handleChangeDate = (date: DayProps) => {
+    const startDate = getStartDate(date);
+    const endDate = getEndDate(date, startDate);
+
+    console.log('handleChangeDate');
+    setLastSelectedDate(endDate);
+    const markedDatesInterval = generateInterval(startDate, endDate);
+    setMarkedDates(markedDatesInterval);
+    return;
   };
 
   return (
@@ -68,7 +104,7 @@ export function Scheduling() {
         </RentalPeriod>
       </Header>
       <Content>
-        <Calendar />
+        <Calendar markedDates={markedDates} onDayPress={handleChangeDate} />
       </Content>
       <Footer>
         <Button title="Confirmar" onPress={handleNavigateToSchedulingDetails} />
